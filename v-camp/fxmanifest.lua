67c3d9af450d370b13501d908163807aefc44252
( Tysit ) <141593443+oquhen@users.noreply.github.com>
2024-01-14 20:57:13 +0300

    Add files via upload

---
+++ v-camp/fxmanifest.lua
@@ -0,0 +1,8 @@
+-- GRODY ROLEPLAY PROJECT SEASON 1 - MAPS
+
+fx_version "adamant"
+game "rdr3"
+
+this_is_a_map "yes"
+
+rdr3_warning "I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships."
